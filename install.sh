432491cc65ff41ac6ed1de10dc26b4fba99039f4
Paul Osborne <paul.osborne.uk@gmail.com>
2014-05-14 15:13:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-  echo === Checking out vundle
-  git submodule update --init
-
-  echo === Linking vim directories
-  ln -s $PWD/.vim/vimrc $HOME/.vimrc
-
-  echo === Vundling
-  vim +BundleInstall +qall
-
-  echo === Done
+git submodule update --init
+ln -s $PWD/.vim/vimrc $HOME/.vimrc
+vim +BundleInstall +qall
