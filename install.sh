e38c0433b2d2bf40f32ed31bca3d6b9d1120db96
Paul Osborne <paul.osborne.uk@gmail.com>
2014-05-14 15:12:59 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
-if [[ -e $HOME/.vimrc || -e $HOME/.vim ]]; then
-  echo "Ooops: ~/.vimrc or ~/.vim already exists. \n\
-       It looks like you already have some vim configuration. \n\
-       Try to run ./uninstall.sh"
-else
+
   echo === Checking out vundle
   git submodule update --init
-  cd -
 
   echo === Linking vim directories
-  ln -s $PWD/vimrc $HOME/.vimrc
-  ln -s $PWD $HOME/.vim
+  ln -s $PWD/.vim/vimrc $HOME/.vimrc
 
   echo === Vundling
   vim +BundleInstall +qall
